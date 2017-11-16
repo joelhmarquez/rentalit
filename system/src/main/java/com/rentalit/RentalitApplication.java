20c6c17dcb77bc7a94672a82e959df5121df0424
Nicl7004 <nicholas.clement@colorado.edu>
2017-11-16 10:48:33 -0700

    first commit with springboot 2.0

---
+++ system/src/main/java/com/rentalit/RentalitApplication.java
@@ -0,0 +1,12 @@
+package com.rentalit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalitApplication.class, args);
+	}
+}
---
+++ system/src/test/java/com/rentalit/RentalitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rentalit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentalitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
