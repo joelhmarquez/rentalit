3f6a41e9c6b60e80a27891e0df34a2295358ddb5
Chris Struckman <chst7928@colorado.edu>
2017-11-20 16:07:24 -0700

    MERGING

--- system/src/main/java/com/rentalit/controllers/RentalitController.java
+++ system/src/main/java/com/rentalit/controllers/RentalitController.java
@@ -1,5 +1,6 @@
 package com.rentalit.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +11,25 @@ import com.rentalit.models.MongoDB;
 @Controller
 public class RentalitController {
 
-    @GetMapping("/post")
-    public String listingForm(Model model) {
-        model.addAttribute("listing", new Listing());
-        return "post";
-    }
+//    @GetMapping("/post")
+//    public String listingForm( Model model) {
+//        model.addAttribute("listing", new Listing());
+//        return "post";
+//    }
+//
+//    @PostMapping("/post")
+//    public String listingSubmit(@ModelAttribute Listing listing) {
+//        MongoDB mongoDB = new MongoDB();
+//        mongoDB.addListing(listing);
+//        return "result";
+//    }
 
-    @PostMapping("/post")
-    public String listingSubmit(@ModelAttribute Listing listing) {
-        MongoDB mongoDB = new MongoDB();
-        mongoDB.addListing(listing);
-        return "result";
+    @RequestMapping(method = RequestMethod.POST, value = "/createListing")
+    ResponseEntity<?> createListing(@RequestBody Listing listing) {
+        try {
+
+        } catch (Error e) {
+
+        }
     }
 }
