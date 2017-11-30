a55dd7c3422c282222c79f1652e52d5e8f86c5ee
Chris Struckman <chst7928@colorado.edu>
2017-11-29 17:12:09 -0700

    Scheduler

--- system/src/main/java/com/rentalit/models/Scheduler.java
+++ system/src/main/java/com/rentalit/models/Scheduler.java
@@ -8,13 +8,14 @@ public class Scheduler {
             Calendar schedule = new Calendar();
             schedule.setStartDate(start_Date);
             schedule.setEnd_Date(end_Date);
-            MongoDB mongo = new MongoDB();
-            Document query = mongo.mongo_Query(listing);
-            List<Listing> item = mongo.search_Item(query);
-            if(item.isEmpty()){
-
-            }else if(item.size() > 1){
-
-            }
+//            MongoDB mongo = new MongoDB();
+//            Document query = mongo.mongo_Query(listing);
+//            List<Listing> item = mongo.search_Item(query);
+//            if(item.isEmpty()){
+//
+//            }else if(item.size() > 1){
+//
+//            }
+            listing.acceptRental(schedule);
     }
 }
