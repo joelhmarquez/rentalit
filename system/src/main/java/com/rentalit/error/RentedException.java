12c5a70957a9f84d416baaa571ddee659e890599
Joel Marquez <joma6391@colorado.edu>
2017-12-02 12:01:12 -0700

    added exception throw from within rented state if rented object is requested

---
+++ system/src/main/java/com/rentalit/error/RentedException.java
@@ -0,0 +1,7 @@
+package com.rentalit.error;
+
+public class RentedException extends Exception{
+	public RentedException(String errorMsg) {
+		super(errorMsg);
+	}
+}
--- system/src/main/java/com/rentalit/models/RentedState.java
+++ system/src/main/java/com/rentalit/models/RentedState.java
@@ -1,14 +1,21 @@
 package com.rentalit.models;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
+import com.rentalit.error.RentedException;
 
 @Configurable
 public class RentedState implements ProductState {
+	static Logger log = LoggerFactory.getLogger(RentedState.class);
+	
 	MongoDB mongo = new MongoDB();
 
 	@Override
-	public void acceptRental(Listing listing) {
-		// TODO Auto-generated method stub
+	public void acceptRental(Listing listing) throws RentedException {
+		log.error("Attempting to checkout rented item:" + listing.getProductName());
+		
+		throw new RentedException(listing.getProductName() + "is already rented");
 	}
 	
 	@Override
